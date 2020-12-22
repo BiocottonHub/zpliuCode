15c1a3686f3fac8a91c197b4c3b19123faa21a99
zpliu1126 <1944532210@qq.com>
2020-12-22 21:54:42 +0800

    dict get somethings error

--- transcriptSV/getProductiveTranscript.py
+++ transcriptSV/getProductiveTranscript.py
@@ -4,7 +4,7 @@ version:
 Author: zpliu
 Date: 2020-12-22 19:50:47
 LastEditors: zpliu
-LastEditTime: 2020-12-22 20:12:28
+LastEditTime: 2020-12-22 21:52:27
 @param: 
 '''
 import sys
@@ -26,13 +26,15 @@ def getProdectiveTranscript(transcriptExpression: str):
             out[line[0]] = out.get(line[0], [line[1], float(
                 line[DeteeminColumn]), float(line[RNAseqColumn])])
             if float(line[DeteeminColumn]) > out[line[0]][1]:
-                out[line[0]] = out.get(line[0], [line[1], float(
-                    line[DeteeminColumn]), float(line[RNAseqColumn])])
+                out[line[0]] = [line[1], float(
+                    line[DeteeminColumn]), float(line[RNAseqColumn])]
             elif float(line[DeteeminColumn]) == out[line[0]][1] and float(line[RNAseqColumn]) >= out[line[0]][2]:
-                out[line[0]] = out.get(line[0], [line[1], float(
-                    line[DeteeminColumn]), float(line[RNAseqColumn])])
+                out[line[0]] = [line[1], float(
+                    line[DeteeminColumn]), float(line[RNAseqColumn])]
+            else:
+                pass
     return out
 
 
 if __name__ == "__main__":
-    print(getProdectiveTranscript(sys.argv[1])['Ghir_A01G000100'])
+    print(getProdectiveTranscript(sys.argv[1])['Ghir_A01G002010'])
