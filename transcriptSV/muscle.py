248409aa81f589b210f932ccd82913b590a8a46d
zpliu1126 <1944532210@qq.com>
2020-12-23 21:24:35 +0800

    修改正则匹配规则

--- transcriptSV/muscle.py
+++ transcriptSV/muscle.py
@@ -4,7 +4,7 @@ version: 1.0
 Author: zpliu
 Date: 2020-12-22 18:57:03
 LastEditors: zpliu
-LastEditTime: 2020-12-22 20:43:57
+LastEditTime: 2020-12-23 20:50:55
 @param: 
 '''
 import os
@@ -25,17 +25,20 @@ def runMuscle(Atranscript: str, Btranscript: str):
     #except
     (0,2)
     '''
-    Deletpattern = r'[ATCGatcgNn]+(-*)[ATCGatcgNn]+'
+    # flank sequence more than 20bp and not greedy model
+    Deletpattern = '[ATCG]{20,}?(-+)[ATCG]{20,}?'
     infasta = Atranscript+"\n"+Btranscript+"\n"
     out = os.popen(
         "printf \"%s\"|~/software/muscle3.8.31_i86linux64   2>/dev/null" % infasta).read()  # get a generator
     Aaligint, Baligint = out.split("\n>")
+    #
+    def f(x): return x if x else '0'
     # 76,45 mean there is 75bp、45bp delet compare with another transctripts
     AdeletCount = ",".join([str(len(i)) for i in re.findall(
         Deletpattern, Aaligint.replace("\n", ""))])  # get the delte count or 0
     BdeletCount = ",".join([str(len(i)) for i in re.findall(
         Deletpattern, Baligint.replace("\n", ""))])
-    return (AdeletCount, BdeletCount)
+    return (f(AdeletCount), f(BdeletCount))
 
 
 def readFastaFile2(fastaFile: str):
